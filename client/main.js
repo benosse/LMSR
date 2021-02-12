b11c770c498978709b46b9c012ce2773d02389da
Benoit <benoit.zenker@gmail.com>
2021-02-12 22:25:31 +0100

    crea projet

---
+++ client/main.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor' 
+import Vue from 'vue'
+
+import App from './components/App.vue'
+
+Meteor.startup(() => {
+    new Vue({
+
+        el: '#app',
+        ...App,
+
+    })
+
+})
