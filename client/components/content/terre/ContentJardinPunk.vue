35c58007b905a24354f7b2fd2b94171f9d471d9c
benoit <benoit.zenker@gmail.com>
2021-06-07 16:25:44 +0200

    début mise en page jardin punk

--- client/components/content/terre/ContentJardinPunk.vue
+++ client/components/content/terre/ContentJardinPunk.vue
@@ -4,7 +4,109 @@
 		<h1 class="width2 left33 gutter">
 			<span class="underlined">Jardin punk</span>
 		</h1>
-		
+
+		<div class="align-top">
+			<div class="flex4"></div>
+
+			<div class="flex2 gutter">
+				<p class="btp-regular">
+					En travaillant sur la Biennale du design de Saint-Etienne, un groupe
+					d'étudiants découvre un terrain vague entre les murs de la Cité du
+					Design. De ce sol abandonné, ne pourrait-on pas faire un beau jardin?
+					Mais si l’on est conscient de l’histoire d’industrialisation et de
+					pollution de Saint-Etienne, est-ce possible ou même souhaitable? De
+					ces questionnements est né le projet du “jardin punk”, dont
+					l’intention est d’explorer le sol depuis différentes disciplines comme
+					la biologie, l’ingénierie, la sociologie, les arts ou d’autres encore.
+					Inspiré par le Petit traité du Jardin Punk écrit par Eric Lenoir, ce
+					projet s’inscrit d'abord dans une démarche d’observation et de
+					compréhension du sol. Cette analyse de terrain, en tant que sol et
+					territoire, peut ensuite informer une cartographie des possibles,
+					c'est-à-dire une cartographie des actions envisageables en termes de
+					dépollution, de réhabilitation, de renaturation, mais aussi
+					d'éducation ou de vie en communauté. Entre médiation, expérimentation,
+					action citoyenne et lien social, le jardin punk souhaite s’inscrire
+					dans le cadre des valeurs et des pratiques de l'interdisciplinarité et
+					de la science ouverte.
+				</p>
+				<p class="btp-light">
+					Au printemps 2021, une première série d'ateliers consacrés à la santé
+					du sol, et à la vulgarisation de ces sujets, a été réalisée entre
+					chercheurs, enseignants et étudiants de l'École des Mines et du CRI.
+					Une recherche sur le terrain de cartographie des plantes est prévue
+					pour juin 2021 par les étudiants de CRI et le pôle terre de l’ESADSE.
+				</p>
+			</div>
+		</div>
+
+		<!-- separation -->
+		<img src="/images/misc/separator3.png" class="separator width3 left16" />
+
+		<div class="align-top">
+			<div class="flex4">
+				<div class="width3 left25">
+					<iframe
+						width="100%"
+						height="450"
+						src="https://www.youtube.com/embed/B2lREA632qc"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowfullscreen
+					></iframe>
+
+					<img src="/images/terre/jardinPunk/media1.gif" />
+
+					<img src="/images/terre/jardinPunk/media2.png" />
+
+					<video controls>
+						<source
+							src="/images/terre/jardinPunk/media3.mov"
+							type="video/mp4"
+						/>
+						Sorry, your browser doesn't support embedded videos.
+					</video>
+				</div>
+			</div>
+
+			<div class="flex2 gutter">
+				<h1>
+					une méthodologie de dialogue et de collaboration pluridisciplinaire
+				</h1>
+				<p class="btp-regular">
+					<a href="">lien vers les pdf...</a>
+				</p>
+			</div>
+		</div>
+
+		<!-- separation -->
+		<img
+			src="/images/misc/separator2.png"
+			class="gutter separator width2 left66 flip"
+		/>
+
+		<div class="align-top">
+			<div class="flex3 gutter">
+				<div class="width2 left33 gutter">
+					<h1>
+						une démarche de science citoyenne: ouverte, accessible, vulgarisée,
+						reproductible par le grand public
+					</h1>
+					<p class="btp-regular">
+						<a href="">lien vers les pdf...</a>
+					</p>
+				</div>
+			</div>
+
+			<div class="flex3 gutter">
+				<img src="/images/terre/jardinPunk/media4.gif" />
+				<iframe
+					src="/images/terre/jardinPunk/media5.pdf"
+					width="100%"
+					height="800px"
+				></iframe>
+			</div>
+		</div>
 	</div>
 </template>
 
